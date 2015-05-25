12e679c34864b4babe8b7be328439b9b01d3a0ad
Alexandru Bondor <alexandru.viorel.bondor@gmail.com>
2015-05-25 11:40:16 -0400

    fb2

--- resources/views/admin/compose.blade.php
+++ resources/views/admin/compose.blade.php
@@ -5,27 +5,4 @@
 @endsection
 
 @section('footer')
-<script type="text/javascript">
-	// var access_token="{{ getenv("FACEBOOK_CLIENT_ID") }}|{{ getenv("FACEBOOK_CLIENT_SECRET") }}";
-	// var template="{{ $logged_user->name }} posted a comment on {{ $group->destination->slug }} - {{ $group->date->format('d.m.y') }} group www.goo.gl";
-	// var callback="#"; //http://www.staging.grouptickets.nl/groups/{{ $group->id }}"
-
-	// var users = {!! $group->users !!};
-	// $( document ).ready(function() {
- //    	 	$('#newComment').on( 'submit', function() {
-			
-	// 		return false;
-	// 	});
-	// });
-//	alert(st);
-//	var gi = $('#group_id').val();
-
-//	var st = "/groups/listen/" + gi;
-	
-//	setInterval(function(){
-//    $.ajax({ url: st, success: function(data){
-//        console.log(data);
-//    }, dataType: "json"});
-//}, 1000);
-</script>
 @endsection
